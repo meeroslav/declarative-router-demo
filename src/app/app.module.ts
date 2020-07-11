16520f6cee557b6aa556d87151e8ab25e3007333
Miroslav Jonas <missing.manual@gmail.com>
2020-07-11 21:10:20 +0200

    feat: add router and cleanup styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RoutingModule } from 'angular-routing';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
